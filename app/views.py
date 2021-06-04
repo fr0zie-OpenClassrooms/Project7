4d5527d71f9a012e814557bb29013b3915b77ee2
fr0zie <axel.rayet@outlook.com>
2021-06-04 08:59:38 +0200

    Added classes and tests

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/answer')
+def answer():
+    pass
---
+++ config.py
@@ -0,0 +1,10 @@
+APP = {
+    'name': 'GrandPy Bot',
+    'author': 'Axel Rayet',
+    'repository': 'https://www.github.com/fr0zie-OpenClassrooms/Project7'
+}
+
+MAPS_API = {
+    'api_key': 'AIzaSyCgY7pboxS5SYH0rYfaePBWNgSwBLy2_Sg',
+    'url': 'https://maps.googleapis.com/maps/api/geocode/json'
+}
